fc670d4622c2c8bc804fa4cbf3ec6bfa2c82c2b5
Folt <122365476+aleksfolt@users.noreply.github.com>
2024-11-04 01:22:35 +0200

    Delete gpt_client.py

--- gpt_client.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-question = 'Who are you?'
-dictToSend = [{"role": "user", "content": question}]
-res = requests.post('http://api.onlysq.ru/ai/v1', json=dictToSend)
-response = res.json()
-print(response['answer'])
